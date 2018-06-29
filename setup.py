8dd69b14131bf6004973e0a2aca08bcfabfe3a81
Gelbpunkt <38864617+Gelbpunkt@users.noreply.github.com>
2018-06-29 13:18:22 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="async-mediawiki",
+    name="async_mediawiki",
     version="0.0.1",
     author="Jens Reidel",
     author_email="jens.reidel@gmail.com",
