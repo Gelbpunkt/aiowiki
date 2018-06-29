ba19fc78b0cd4ea0f14f58eb85f367a120a59807
Gelbpunkt <38864617+Gelbpunkt@users.noreply.github.com>
2018-06-29 12:44:35 +0200

    Update mediawiki.py

--- mediawiki.py
+++ mediawiki.py
@@ -21,15 +21,42 @@ class MediaWiki:
         cleantext = cleantext.replace("[edit]", "")
         cleantext = cleantext.replace("(edit)", "")
         return cleantext
-
-    async def get(self, pageTitle:str):
-        """Get a page content. Either from URL or a page in the URL from the constructer."""
+    
+    async def _html(self, pageTitle:str):
+        """Helper function that downloads the page HTML."""
         if not self.baseUrl:
             url = pageTitle
         else:
             url = f"{self.baseUrl}?action=parse&page={pageTitle}&format=json"
         async with self.session.get(url) as r:
             data = await r.json()
+        return data["parse"]["text"]["*"]
 
-        data = data["parse"]["text"]["*"]
+    async def _markdown(self, pageTitle:str):
+        """Helper function to get page markdown."""
+        if not self.baseUrl:
+            url = pageTitle
+        else:
+            url = f"{self.baseUrl}?action=query&titles={pageTitle}&prop=revisions&rvprop=content&format=json&formatversion=2"
+        async with self.session.get(url) as r:
+            data = await r.json()
+        return data["query"]["pages"][0]["revisions"][0]["content"]
+    
+    async def get_text(self, pageTitle:str):
+        """Get a page content. Either from URL or a page in the URL from the constructer."""
+
+            
+
+
+
+        data = await self._html(pageTitle)
+
+
         return self._cleanhtml(data)
+    
+    async def get_html(self, pageTitle:str):
+        """Get the raw page HTML. Either from URL or page in defined wiki."""
+        return await self._html(pageTitle)
+    
+    async def get_markdown(self, pageTitle:str):
+        return await self._markdown(pageTitle)
