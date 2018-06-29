2eb47586799cd5737b8047c83f47afd15dd4cb27
Gelbpunkt <38864617+Gelbpunkt@users.noreply.github.com>
2018-06-29 13:15:23 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="async-mediawiki",
+    version="0.0.1",
+    author="Jens Reidel",
+    author_email="jens.reidel@gmail.com",
+    description="Async Mediawiki python libary",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Gelbpunkt/async-mediawiki",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
